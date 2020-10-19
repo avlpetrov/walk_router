9eca944091b914caa99ec92070783a164d6c091a
Anton Petrov <anton.petrov.swe@gmail.com>
2020-10-20 02:13:59 +0300

    Add some help description to CLI

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ from src.utils import validate_file
 @click.argument("from_node_id")
 @click.argument("to_node_id")
 def main(path: Path, from_node_id: NodeID, to_node_id: NodeID) -> None:
+    """Finds shortest distance between nodes in graph if path exists."""
     graph = UndirectedGraph.build_from_file(path)
     shortest_distance = graph.find_shortest_distance(from_node_id, to_node_id)
     if isinf(shortest_distance):
